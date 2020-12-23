61bf6c1f8e982a0c408d8f42c651e5b55d566e3d
eulerblind <elvisiky@163.com>
2020-12-23 17:33:20 +0800

    添加readme

---
+++ readme.md
@@ -0,0 +1,14 @@
+# 运行方式
+    ```
+    uvicorn sha256_api:app --workers=4 --port=8000 --host=0.0.0.0
+    ```
+
+# 提供两个API
+    1、`GET /sha256`直接返回请求体的sha256值
+    2、`POST /sha256` 在json请求体中传入raw_string参数，返回的json请求体中sha256字段即sha256值
+
+# 调试界面
+    `http://elvisiky.com:8033/docs`
+
+# 部署方式
+    ```docker-compose up -d --build```
--- sha256_api.py
+++ sha256_api.py
@@ -1,11 +1,37 @@
 from fastapi import FastAPI, Request
 import hashlib
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class Sha256Item(BaseModel):
+    raw_string: str
+
+
+class Sha256Response(BaseModel):
+    sha256: str
+
+
+@app.post("/sha256", response_model=Sha256Response)
+async def get_sha256_value(body: Sha256Item):
+    """
+    :param body: json 类型
+    :return:
+    """
+    content = body.raw_string
+    m_sha256 = hashlib.sha256()
+    m_sha256.update(bytes(content, "utf8"))
+    resp = Sha256Response(sha256=m_sha256.hexdigest())
+    return resp
+
+
 @app.get("/sha256")
 async def get_sha256_value(request: Request):
+    """
+    :param request: 单请求体类型
+    :return:
+    """
     content = await request.body()
     m_sha256 = hashlib.sha256()
     m_sha256.update(content)
@@ -15,4 +41,4 @@ async def get_sha256_value(request: Request):
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(app,work=4)
+    uvicorn.run(app)
